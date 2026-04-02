8a8434187605e5ebce75cb4d98659ae2eccd9b5d
sanyedh <112471631+sanyedh@users.noreply.github.com>
2026-04-02 22:09:15 +0800

    Delete segmentation directory

--- segmentation/denseclip/__init__.py
+++
@@ -1,3 +0,0 @@
-from .denseclip import DenseCLIP
-from .models import CLIPResNet, CLIPTextEncoder, CLIPVisionTransformer, CLIPResNetWithAttention
-from .heads import IdentityHead
--- segmentation/mmseg_custom/datasets/__init__.py
+++
@@ -1,1 +0,0 @@
-from .deepfake import DeepfakeDataset
